8090496333921d2e691dece3a0ded98a8480b76d
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2019-03-22 19:25:11 +0100

    Added Camera and Player

---
+++ HighSpeedSkating/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Camera Target")]
+    public Transform target;
+
+    [Header("Camera Settings")]
+    public Vector3 offset;
+
+    private float counter;
+
+
+    [Header("Camera FX")]
+    public CameraShaker.Properties shakerProperties;
+
+    private void Start()
+    {
+        transform.position = offset;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+        transform.LookAt(target.position);
+    }
+
+    public void StandardCameraShake()
+    {
+        FindObjectOfType<CameraShaker>().StartShake(shakerProperties);
+    }
+
+    public void ShakeCamera(CameraShaker.Properties shakeProperties)
+    {
+        FindObjectOfType<CameraShaker>().StartShake(shakeProperties);
+    }
+}
