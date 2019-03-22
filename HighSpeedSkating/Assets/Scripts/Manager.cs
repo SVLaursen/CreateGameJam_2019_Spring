16228f8720bc65feb4888ef825a1da14bdba9b3c
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2019-03-22 23:41:44 +0100

    Camera Effects!

---
+++ HighSpeedSkating/Assets/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private PlayerController player;
+
+    public Canvas pauseScreen;
+    public Canvas gameOverScreen;
+    public Canvas uiCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
